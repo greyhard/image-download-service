e36545fe696b97890979c504989891818d82f0c1
greyhard <greyhard@gmail.com>
2019-02-13 23:48:32 +0300

    Подключение списка socks5 проксей для парсинга

--- upload/main.go
+++ upload/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+    "bufio"
     "encoding/json"
+    "errors"
     "flag"
     "fmt"
     "github.com/disintegration/imaging"
@@ -16,17 +18,60 @@ import (
     "os"
     "path/filepath"
     "strconv"
+    "sync"
     "time"
 )
 
 var (
     httpPort, imageDir *string
     tasks              map[int]Task
+    proxy              []Proxy
+    syncMapMutex       = sync.RWMutex{}
+    hasActiveProxy     = false
+    activeProxy        Proxy
+    proxyLimit         = 20
 )
 
 func main() {
 
     tasks = make(map[int]Task)
+    //proxy = make(map[string]Proxy)
+
+    if err := loadProxy(); err != nil {
+        log.Fatal(err)
+    }
+
+    go func() {
+        for {
+            time.Sleep(60 * time.Second)
+            fmt.Printf("%s Cleanup: \n", time.Now().Format(time.RFC3339))
+        }
+    }()
+
+    go func() {
+        for {
+
+            if !hasActiveProxy {
+
+                if len(proxy) > 0 {
+                    currentProxy := proxy[0] // get the 0 index element from slice
+                    proxy = proxy[1:]        // remove the 0 index element from slice
+                    hasActiveProxy = true
+                    activeProxy = currentProxy
+                    fmt.Printf("%s Use proxy: %s Left %d \n", time.Now().Format(time.RFC3339), activeProxy.Ip, len(proxy))
+                } else {
+                    if err := loadProxy(); err != nil {
+                        log.Fatal(err)
+                    }
+                }
+            }
+
+            fmt.Printf("%s Proxy checker\n", time.Now().Format(time.RFC3339))
+            time.Sleep(5 * time.Second)
+
+        }
+
+    }()
 
     httpPort = flag.String("port", "", "Port")
     imageDir = flag.String("dir", "", "Path")
@@ -41,6 +86,7 @@ func main() {
     fmt.Printf("%s http://:%s\n", time.Now().Format(time.RFC3339), *httpPort)
     fmt.Printf("%s Upload dir: %s\n", time.Now().Format(time.RFC3339), *imageDir)
     _ = http.ListenAndServe(":"+*httpPort, r)
+
 }
 
 type Images struct {
@@ -50,6 +96,12 @@ type Images struct {
 type Image struct {
     Url  string `json:"image"`
     Crop bool   `json:"crop"`
+}
+
+type Proxy struct {
+    Ip    string
+    Port  int
+    Usage int
 }
 
 type Task struct {
@@ -81,15 +133,15 @@ func doCreateTask(w http.ResponseWriter, r *http.Request) {
 
     taskId := rand.Int()
 
-
-
     var resp = Task{
         Images: images.Images,
         TaskId: taskId,
         Status: "inprogress",
     }
 
+    syncMapMutex.Lock()
     tasks[taskId] = resp
+    syncMapMutex.Unlock()
 
     go func() {
 
@@ -103,6 +155,7 @@ func doCreateTask(w http.ResponseWriter, r *http.Request) {
 
             if err != nil {
                 fmt.Printf("%s Downloaderror2\n", time.Now().Format(time.RFC3339))
+                log.Println(err)
             }
             if err == nil {
 
@@ -141,7 +194,9 @@ func doCheckTask(w http.ResponseWriter, r *http.Request) {
         _ = json.NewEncoder(w).Encode(resp)
 
         if resp.Status == "ready" {
+            syncMapMutex.Lock()
             delete(tasks, taskId)
+            syncMapMutex.Unlock()
         }
 
     } else {
@@ -216,6 +271,10 @@ func doFetchImage(image Image) (err error, out string){
             var src image2.Image
             src, err = imaging.Open(folderPath + file)
 
+            if err != nil {
+                return err, ""
+            }
+
             //Получаем размер изображения
             imageSize := src.Bounds()
             imgWidth := imageSize.Max.X
@@ -242,15 +301,47 @@ func doFetchImage(image Image) (err error, out string){
     return nil, u.String()
 }
 
-func downloadFile(filepath string, url string) (err error) {
+func downloadFile(filepath string, imageUrl string) (err error) {
+
+    if !hasActiveProxy {
+        return errors.New("noActiveProxy")
+    }
+
+    activeProxy.Usage = activeProxy.Usage + 1
+
+    if activeProxy.Usage > proxyLimit {
+        hasActiveProxy = false
+        activeProxy = Proxy{}
+        return errors.New("proxyLimitReached")
+    } else {
+        fmt.Printf("%s Proxy Usage %s: %d < %d\n",
+            time.Now().Format(time.RFC3339), activeProxy.Ip,
+            activeProxy.Usage, proxyLimit)
+    }
+
+    if activeProxy.Ip == "" {
+        hasActiveProxy = false
+        activeProxy = Proxy{}
+        return errors.New("noProxyIpDefined")
+    }
 
     // Get the data
-    fmt.Printf("%s Download Data: %s\n", time.Now().Format(time.RFC3339), url)
+    fmt.Printf("%s Download Data: %s\n", time.Now().Format(time.RFC3339), imageUrl)
 
-    if response, err := http.Get(url); err != nil {
+    proxyUrl, err := url.Parse(fmt.Sprintf("socks5://%s", activeProxy.Ip))
+    timeout := time.Duration(10 * time.Second)
+
+    proxy := &http.Client{
+        Transport: &http.Transport{Proxy: http.ProxyURL(proxyUrl)},
+        Timeout:   timeout,
+    }
+
+    if response, err := proxy.Get(imageUrl); err != nil {
+        hasActiveProxy = false
+        activeProxy = Proxy{}
         fmt.Printf("%s Download Error\n", time.Now().Format(time.RFC3339))
-
         return err
+
     } else {
 
         fmt.Printf("%s Responce: %v   Error: %v\n", time.Now().Format(time.RFC3339), response, err)
@@ -258,37 +349,70 @@ func downloadFile(filepath string, url string) (err error) {
 
         tTime := time.Now()
 
-        if err != nil {
-            fmt.Println("Hello")
-        } else {
-            if response.StatusCode == 200 {
-                fmt.Println(tTime.Format(time.RFC3339), "OK")
+        if response.StatusCode == 200 {
+            fmt.Println(tTime.Format(time.RFC3339), "OK")
 
-                println(time.Now().Format(time.RFC3339), "Create File:", filepath)
+            println(time.Now().Format(time.RFC3339), "Create File:", filepath)
 
-                out, err := os.Create(filepath)
-                if err != nil {
-                    return err
-                }
-
-                fmt.Printf("%s Write File: %s\n", time.Now().Format(time.RFC3339), filepath)
-
-                _, err = io.Copy(out, response.Body)
-                if err != nil {
-                    _ = os.Remove(filepath)
-                    return err
-                }
-
-                defer out.Close()
-
-            } else {
-                fmt.Println(tTime.Format(time.RFC3339), "BAD")
-                _ = os.Remove(filepath)
-                return fmt.Errorf("bad status: %s", response.Status)
+            out, err := os.Create(filepath)
+            if err != nil {
+                return err
             }
+
+            fmt.Printf("%s Write File: %s\n", time.Now().Format(time.RFC3339), filepath)
+
+            _, err = io.Copy(out, response.Body)
+            if err != nil {
+                _ = os.Remove(filepath)
+                return err
+            }
+
+            defer out.Close()
+
+        } else {
+            fmt.Println(tTime.Format(time.RFC3339), "BAD")
+            _ = os.Remove(filepath)
+            return fmt.Errorf("bad status: %s", response.Status)
         }
 
     }
 
     return nil
+}
+
+func loadProxy() (err error) {
+
+    if file, err := os.Open("proxy.txt"); err == nil {
+
+        scanner := bufio.NewScanner(file)
+        for scanner.Scan() {
+            fmt.Println(scanner.Text())
+
+            proxy = append(proxy, Proxy{
+                Ip:    scanner.Text(),
+                Port:  9999,
+                Usage: 0,
+            })
+
+        }
+
+        proxy = Shuffle(proxy)
+
+        return nil
+
+    }
+
+    return errors.New("cantOpenProxyFile")
+}
+
+func Shuffle(vals []Proxy) []Proxy {
+    r := rand.New(rand.NewSource(time.Now().Unix()))
+    ret := make([]Proxy, len(vals))
+    n := len(vals)
+    for i := 0; i < n; i++ {
+        randIndex := r.Intn(len(vals))
+        ret[i] = vals[randIndex]
+        vals = append(vals[:randIndex], vals[randIndex+1:]...)
+    }
+    return ret
 }
